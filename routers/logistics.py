49d359754970d493d08e4f0d4fce8f1f1bea359a
Mensah Nathaniel Justice Kojo <nathanieljusticemensah@gmail.com>
2026-07-01 14:09:46 +0000

    Add . added logistics.py

--- routers/logistics.py
+++ routers/logistics.py
@@ -1,1 +1,19 @@
 #routs for the logistics
+from fastapi import APIRouter
+from models.schemas import CostEstimateRequest
+
+router = APIRouter(prefix="/api")
+
+@router.post("/estimate-cost")
+async def estimate_transport_cost(request: CostEstimateRequest):
+    # TODO: Load XGBoost .pkl model here
+    # TODO: Feed distance and payload_kg into the model to get predicted price
+    
+    # Placeholder response
+    estimated_price = 150.00 # Mock price in GHS
+    
+    return {
+        "status": "success",
+        "estimated_cost_ghs": estimated_price,
+        "currency": "GHS"
+    }
